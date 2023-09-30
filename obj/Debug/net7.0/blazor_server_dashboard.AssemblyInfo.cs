1e33c661fd606b703a29deebd841a4c0cc7497d3
Neil Ranada <neil.ranada@gmail.com>
2023-09-30 16:42:27 -0700

    Merged old state with last and first names as only additional columns in database

--- obj/Debug/net7.0/blazor_server_dashboard.AssemblyInfo.cs
+++ obj/Debug/net7.0/blazor_server_dashboard.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor_server_dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67a40ef205b3722ce08cbfc6bb114ecef6062ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor_server_dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor_server_dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
