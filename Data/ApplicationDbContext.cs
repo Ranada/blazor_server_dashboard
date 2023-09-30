b616f26ebe0df0b528a58d0f42d018d91ef69da2
Neil Ranada <neil.ranada@gmail.com>
2023-09-30 04:32:48 -0700

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace blazor_server_dashboard.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
