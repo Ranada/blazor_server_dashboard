5ba561610a9fcae0b2a408144145cf882c13afd9
Neil Ranada <neil.ranada@gmail.com>
2023-10-02 16:39:48 -0400

    Update tile placeholder data

--- obj/Debug/net8.0/blazor_server_dashboard.AssemblyInfo.cs
+++ obj/Debug/net8.0/blazor_server_dashboard.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor_server_dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed0c115b73726e5e22753e17938ac2e955dfd0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor_server_dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor_server_dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
