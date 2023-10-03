1fb23e5d2c4f4c26b9ef8cc7f34b9e199896e767
Neil Ranada <neil.ranada@gmail.com>
2023-10-02 23:04:37 -0400

    Add cascadingparameter to pass data between dashboard components

--- obj/Debug/net8.0/blazor_server_dashboard.AssemblyInfo.cs
+++ obj/Debug/net8.0/blazor_server_dashboard.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor_server_dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef72070fdcf5345f673a55d40189816feb6790d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor_server_dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor_server_dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
