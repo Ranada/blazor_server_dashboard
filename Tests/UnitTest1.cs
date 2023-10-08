eefb174e96caece97f60ef268f9b2e5190bbe4ce
Neil Ranada <neil.ranada@gmail.com>
2023-10-08 16:56:59 -0500

    Add packages for xUnit and comment out duplicate references

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace blazor_server_dashboard.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Test 1");
+        }
+    }
+}
